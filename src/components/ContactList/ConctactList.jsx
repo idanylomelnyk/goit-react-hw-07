79112a41c23b29a6889f0bd879039132f93cc709
Danylo <idanylomelnyk@gmail.com>
2024-05-24 21:21:46 +0300

    contactlist

--- src/components/ContactList/ConctactList.jsx
+++ src/components/ContactList/ConctactList.jsx
@@ -1,17 +1,13 @@
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
-import { selectFilteredContacts, selectLoading, selectError } from "../../redux/contactsSlice";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 
 export default function ContactList() {
   const filteredContacts = useSelector(selectFilteredContacts);
-  const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
 
   return (
     <>
-      {loading && <p>Loading...</p>}
-      {error && <p className={css.error}>Error: {error}</p>}
       <ul className={css.list}>
         {filteredContacts.map((contact) => (
           <li className={css.item} key={contact.id}>
